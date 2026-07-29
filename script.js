8f00b9cc0e43ad5e64c9e24f24be7ab16b7bbbf8
AerynEnyo <142051937+AerynEnyo@users.noreply.github.com>
2026-07-28 22:31:11 -0230

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
+// ===== Witness List =====
 
+const witnesses = [
+
+    {
+        name: "Witness #1",
+        thumb: "images/Case1.png",
+        image: "images/Case1Opened.png"
+    }
+
+];
+
+// ===============================
+
+const witnessList = document.querySelector(".witnessList");
+
+const popup = document.getElementById("imagePopup");
+const popupImage = document.getElementById("popupImage");
+const closePopup = document.getElementById("closePopup");
+
+witnesses.forEach(witness => {
+
+    const button = document.createElement("button");
+
+    button.className = "witnessBtn";
+
+    button.innerHTML = `
+        <img src="${witness.thumb}">
+        <span>${witness.name}</span>
+    `;
+
+    button.addEventListener("click", () => {
+
+        popupImage.src = witness.image;
+        popup.style.display = "flex";
+
+    });
+
+    witnessList.appendChild(button);
+
+});
+
+closePopup.onclick = () => {
+    popup.style.display = "none";
+};
+
+popup.onclick = e => {
+
+    if(e.target === popup)
+        popup.style.display = "none";
+
+};
